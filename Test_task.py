63cf91151eacf3384d5df94c9f721ce6b26f8f94
Vyacheslav <nesterov_kotelnikovo@mail.ru>
2023-04-23 13:53:25 +0300

    Finished work

--- Test_task.py
+++ Test_task.py
@@ -4,88 +4,43 @@ import pandas as pd
 import numpy as np
 import time
 
-model = torch.hub.load("ultralytics/yolov5", "yolov5s", pretrained=True)
 
-
-def one_img():
-    img = "screen1.png"
-
-    results = model(img)
-    results.print()
-    print("\n\n\n")
-
-    df = pd.DataFrame(results.pandas().xyxy[0], )
-    print(df)
-
-    indexes = df[df['class'] != 0].index
-    df.drop(indexes, inplace=True)
-    df.reset_index(drop=True, inplace=True)
-    print(df)
-
-    # for i in range(df.shape[0]):
-    #     print(df['xmin'][i], ':', df['ymin'][i])
-    #     print(df['xmax'][i], ':', df['ymax'][i])
-
-    x1, y1 = int(df['xmin'][0]), int(df['ymin'][0])
-    x2, y2 = int(df['xmax'][0]), int(df['ymax'][0])
-    image = cv.imread(img)
-    mask = np.zeros_like(image)
-    cv.rectangle(mask, (x1, y1), (x2, y2), (255, 255, 255), -1)
-
-    img_masked = cv.bitwise_and(image, mask)
-
-    cv.imshow('masked', img_masked)
-    cv.waitKey(0)
-
-
-def handler(frame):
+def handler(frame, class_name, model):
     detected_frame = model(frame)
     df = pd.DataFrame(detected_frame.pandas().xyxy[0])
 
-    indexes = df[df['class'] != 2].index
+    # Удаление записей с не заданным пользователем именем класса
+    indexes = df[df['name'] != class_name].index
     df.drop(indexes, inplace=True)
     df.reset_index(drop=True, inplace=True)
 
+    # Если нужного объекта в кадре нет, возвращается обычный кадр
     if (df.empty):
         return frame
 
+    # Координаты прямоугольной рамки
     x1, y1 = int(df['xmin'][0]), int(df['ymin'][0])
     x2, y2 = int(df['xmax'][0]), int(df['ymax'][0])
-    image = frame
 
-    mask = np.zeros_like(image)
+    # Маска для прямоугольника в кадре
+    mask = np.zeros_like(frame)
     cv.rectangle(mask, (x1, y1), (x2, y2), (255, 255, 255), -1)
+    frame_masked = cv.bitwise_and(frame, mask)
 
-    img_masked = cv.bitwise_and(image, mask)
-
-    return img_masked
-
-
-def video_capture():
-    cap = cv.VideoCapture(0)
-
-    while True:
-        ret, frame = cap.read()
-
-        if not ret:
-            break
-
-        frame = handler(frame)
-        cv.imshow('video', frame)
-
-        key = cv.waitKey(1)
-        if key == 27:
-            break
-
-    cap.release()
-    cv.destroyAllWindows()
+    return frame_masked
 
 
-def video_processing():
-    video_path = "drift.mp4"
-    output_name = "MyVideo.mp4"
+def video_processing(video_path, output_name, class_name, model):
     cap = cv.VideoCapture(video_path)
 
+    if not cap.isOpened():
+        print("Видеофайл не открылся!")
+        print("Повторите ВВОД!")
+        return True
+
+    print("Видеофайл открылся, выполняется обработка...")
+
+    # Параметры для записи видеофайла
     width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
     fps = int(cap.get(cv.CAP_PROP_FPS))
@@ -96,22 +51,37 @@ def video_processing():
                          fps, (width, height),
                          isColor=True)
 
+    # Покадровая обработка и запись
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
 
-        frame = handler(frame)
+        frame = handler(frame, class_name, model)
         out.write(frame)
 
     cap.release()
     cv.destroyAllWindows()
+    return False
+
+
+def main():
+    model = torch.hub.load("ultralytics/yolov5", "yolov5s", pretrained=True)
+
+    output_name = "ResultVideo.mp4"
+    class_name = input("Введите имя класса объекта: ")
+
+    flag = True
+    while (flag):
+        video_path = input("Введите путь к видеофайлу: ")
+
+        begin_time = time.time()
+        flag = video_processing(video_path, output_name, class_name, model)
+        end_time = time.time()
+
+    print(f"Обработка видео заняла: {end_time - begin_time} сек.")
+    print(f"Новый видеофайл сохранён в директорию программы с именем {output_name}")
 
 
 if __name__ == "__main__":
-    # one_img()
-    # video_capture()
-    begin_time = time.time()
-    video_processing()
-    end_time = time.time()
-    print(f"Video processing time: {end_time - begin_time}")
+    main()
